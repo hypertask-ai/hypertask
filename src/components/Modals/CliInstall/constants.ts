44bb6f300248270c748f2c9046304c40e03b1dde
Valentin Yeo <valentin@hypertask.ai>
2026-08-31 17:27:45 +0200

    HTPR-5762 [BUGFIX] pin the app installer script

--- src/components/Modals/CliInstall/constants.ts
+++ src/components/Modals/CliInstall/constants.ts
@@ -1,4 +1,4 @@
 export const INSTALL_COMMAND =
-  "curl -fsSL https://raw.githubusercontent.com/hypertask-ai/cli/main/scripts/install.sh | sh";
+  "curl -fsSL https://raw.githubusercontent.com/hypertask-ai/cli/8a9245246cec8ddac4ab1dfccae5c5cba5c39707/scripts/install.sh | sh";
 export const LOGIN_COMMAND = "hypertask login";
 export const CLI_DOCS_URL = "https://docs.hypertask.ai";
