8bb076d7f5cfd3bf4812940313640ef5d6a0909b
valtronic <valentin.yeo@gmail.com>
2026-09-07 23:56:47 +0100

    Revert "HTPR-6047: mark node-html-parser callers server-only (#386)" (#390)
    This reverts commit 38fb15487a8a8d423661c3bd10d86b138e9ef772.

--- src/utils/controllers/urls/extractUrlsFromContent.ts
+++ src/utils/controllers/urls/extractUrlsFromContent.ts
@@ -1,4 +1,3 @@
-import 'server-only';
 import { parse, HTMLElement } from 'node-html-parser';
 import { IUrl } from '@/models/model';
 import addIntoTask from '@/utils/controllers/urls/addIntoTask';
